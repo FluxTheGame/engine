01bf83b5497587b4c7ee1088fb0f0885ea5724f7
Jahfer Husain <jahferh@gmail.com>
2012-08-23 23:12:43 -0400

    First Cinder app! Have two-way communication between Cinder and Node.js server. Can send objects with string, float and int types. Will currently only run on OS X. To run yourself, click on the target in Xcode, and under Build Settings the very last setting will be the address to your Cinder folder. Point to where the folder you downloaded from libcinder is.

---
+++ throwaways/Jahfer/NodeJs/xcode/build/NodeJs.build/Debug/NodeJs.build/Objects-normal/i386/OscBundle.d
@@ -0,0 +1,44 @@
+dependencies: \
+  /Volumes/OTTAWA/Senior\ Project/Development/throwaways/Jahfer/NodeJs/xcode/../blocks/osc/src/OscBundle.cpp \
+  /Volumes/OTTAWA/Senior\ Project/Development/throwaways/Jahfer/NodeJs/xcode/../blocks/osc/include/OscBundle.h \
+  /Volumes/OTTAWA/Senior\ Project/Development/throwaways/Jahfer/NodeJs/xcode/../blocks/osc/include/OscMessage.h \
+  /Users/jahferh/Documents/cinder_0.8.4_mac/include/cinder/Cinder.h \
+  /Users/jahferh/Documents/cinder_0.8.4_mac/boost/boost/cstdint.hpp \
+  /Users/jahferh/Documents/cinder_0.8.4_mac/boost/boost/config.hpp \
+  /Users/jahferh/Documents/cinder_0.8.4_mac/boost/boost/config/user.hpp \
+  /Users/jahferh/Documents/cinder_0.8.4_mac/boost/boost/config/select_compiler_config.hpp \
+  /Users/jahferh/Documents/cinder_0.8.4_mac/boost/boost/config/compiler/clang.hpp \
+  /Users/jahferh/Documents/cinder_0.8.4_mac/boost/boost/config/select_stdlib_config.hpp \
+  /Users/jahferh/Documents/cinder_0.8.4_mac/boost/boost/config/no_tr1/utility.hpp \
+  /Users/jahferh/Documents/cinder_0.8.4_mac/boost/boost/config/stdlib/libstdcpp3.hpp \
+  /Users/jahferh/Documents/cinder_0.8.4_mac/boost/boost/config/select_platform_config.hpp \
+  /Users/jahferh/Documents/cinder_0.8.4_mac/boost/boost/config/platform/macos.hpp \
+  /Users/jahferh/Documents/cinder_0.8.4_mac/boost/boost/config/posix_features.hpp \
+  /Users/jahferh/Documents/cinder_0.8.4_mac/boost/boost/config/suffix.hpp \
+  /Users/jahferh/Documents/cinder_0.8.4_mac/boost/boost/version.hpp \
+  /Users/jahferh/Documents/cinder_0.8.4_mac/boost/boost/shared_ptr.hpp \
+  /Users/jahferh/Documents/cinder_0.8.4_mac/boost/boost/smart_ptr/shared_ptr.hpp \
+  /Users/jahferh/Documents/cinder_0.8.4_mac/boost/boost/config/no_tr1/memory.hpp \
+  /Users/jahferh/Documents/cinder_0.8.4_mac/boost/boost/assert.hpp \
+  /Users/jahferh/Documents/cinder_0.8.4_mac/boost/boost/current_function.hpp \
+  /Users/jahferh/Documents/cinder_0.8.4_mac/boost/boost/checked_delete.hpp \
+  /Users/jahferh/Documents/cinder_0.8.4_mac/boost/boost/throw_exception.hpp \
+  /Users/jahferh/Documents/cinder_0.8.4_mac/boost/boost/exception/detail/attribute_noreturn.hpp \
+  /Users/jahferh/Documents/cinder_0.8.4_mac/boost/boost/detail/workaround.hpp \
+  /Users/jahferh/Documents/cinder_0.8.4_mac/boost/boost/exception/exception.hpp \
+  /Users/jahferh/Documents/cinder_0.8.4_mac/boost/boost/smart_ptr/detail/shared_count.hpp \
+  /Users/jahferh/Documents/cinder_0.8.4_mac/boost/boost/smart_ptr/bad_weak_ptr.hpp \
+  /Users/jahferh/Documents/cinder_0.8.4_mac/boost/boost/smart_ptr/detail/sp_counted_base.hpp \
+  /Users/jahferh/Documents/cinder_0.8.4_mac/boost/boost/smart_ptr/detail/sp_has_sync.hpp \
+  /Users/jahferh/Documents/cinder_0.8.4_mac/boost/boost/smart_ptr/detail/sp_counted_base_gcc_x86.hpp \
+  /Users/jahferh/Documents/cinder_0.8.4_mac/boost/boost/detail/sp_typeinfo.hpp \
+  /Users/jahferh/Documents/cinder_0.8.4_mac/boost/boost/smart_ptr/detail/sp_counted_impl.hpp \
+  /Users/jahferh/Documents/cinder_0.8.4_mac/boost/boost/smart_ptr/detail/sp_convertible.hpp \
+  /Users/jahferh/Documents/cinder_0.8.4_mac/boost/boost/smart_ptr/detail/spinlock_pool.hpp \
+  /Users/jahferh/Documents/cinder_0.8.4_mac/boost/boost/smart_ptr/detail/spinlock.hpp \
+  /Users/jahferh/Documents/cinder_0.8.4_mac/boost/boost/smart_ptr/detail/spinlock_sync.hpp \
+  /Users/jahferh/Documents/cinder_0.8.4_mac/boost/boost/smart_ptr/detail/yield_k.hpp \
+  /Users/jahferh/Documents/cinder_0.8.4_mac/boost/boost/memory_order.hpp \
+  /Users/jahferh/Documents/cinder_0.8.4_mac/boost/boost/smart_ptr/detail/operator_bool.hpp \
+  /Users/jahferh/Documents/cinder_0.8.4_mac/include/cinder/Exception.h \
+  /Volumes/OTTAWA/Senior\ Project/Development/throwaways/Jahfer/NodeJs/xcode/../blocks/osc/include/OscArg.h
