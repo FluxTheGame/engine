2a9baa2af11fddd8266279b5fdeda2c236e93c61
Jahfer <devnull@localhost>
2012-11-24 23:55:06 -0500

    Flocking in XNA with 3D models now working (in throwaways/jahfer)

---
+++ throwaways/Jahfer/Flocking/Flocking_002/Flocking_002/Camera.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Flocking_002
+{
+
+    public class Camera : Microsoft.Xna.Framework.GameComponent
+    {
+
+        //camera marticies
+        public Matrix view;
+        public Matrix projection;
+        public BoundingFrustum frustum;
+
+        public Camera(Game game, Vector3 pos, Vector3 target, Vector3 up) : base(game)
+        {
+            view = Matrix.CreateLookAt(pos, target, up);
+            projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(45.0f), game.GraphicsDevice.Viewport.AspectRatio, 1.0f, 10000.0f);
+            frustum = new BoundingFrustum(view * projection);
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+    }
+
+}
