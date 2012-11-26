05133599937dd7df3b997324f5c66a5b7726f3a0
Jahfer <devnull@localhost>
2012-11-25 23:23:36 -0500

    Added multi-monitor throwaway to repo

---
+++ throwaways/Jahfer/MultiMonitor/MultiMonitor/MultiMonitor/Camera.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace MultiMonitor
+{
+    public class Camera
+    {
+        // Set the position of the camera in world space, for our view matrix.
+        public static Vector3 Position = new Vector3(0.0f, 50.0f, 5000.0f);
+
+
+        public Camera()
+        {
+
+        }
+    }
+}
