9f4722e61c534745706fc94fb37e9e413843e535
Jahfer <devnull@localhost>
2013-03-11 13:54:30 -0400

    Added ground file.

---
+++ production/Flux/Flux/Objects/Ground.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Flux
+{
+    public class Ground
+    {
+        public Model model;
+        Vector3 position;
+        Vector3 rotation;
+
+        public Ground()
+        {
+            model = ContentManager.Model(@"env/Ground_Plane");
+            position = Vector3.Zero;
+            rotation = new Vector3(0, 180, 0);
+        }
+
+        public Vector3 Location()
+        {
+            return position;
+        }
+
+        public void Draw()
+        {
+            for (int i = 0; i < ScreenManager.screens; i++)
+            {
+                ScreenManager.SetTarget(i);
+                Camera c = ScreenManager.Camera(i);
+                Drawer3D.Draw(model, Location(), rotation, c);
+            }
+        }
+    }
+}
