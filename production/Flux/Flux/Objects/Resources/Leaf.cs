9236d9b693947f446877939e1e371c698a0a1cdd
pyoung2 <devnull@localhost>
2013-04-04 09:32:14 -0400

    Merge - Added first attempt at resource "point to target". Also changed animated behaviour.

--- production/Flux/Flux/Objects/Resources/Leaf.cs
+++ production/Flux/Flux/Objects/Resources/Leaf.cs
@@ -19,28 +19,43 @@ namespace Flux
 
         private float sinAngle;
         private float intensity;
+        private float spin;
+        private Random random = new Random();
+        private float xOffset;
 
         public Leaf(Vector3 location, string modelName, float speed = 0.05f) : base(location, modelName, speed)
         {
             sinAngle = 0f;
             intensity = 0f;
+            spin = 0f;
+            xOffset = (float)random.Next(0, 20);
         }
 
         protected override void MoveToCollector()
         {
-            sinAngle += 5f + intensity;
-            rotation = new Vector3(10 * ((float)Math.Sin((double)(Math.PI * (sinAngle + 30) / 180))), 0f, 45 * ((float)Math.Sin((double)(Math.PI * sinAngle / 180))));
-
+            Vector2 difference = collector.position - position;
+            double rad = Math.Atan2((difference.Y), (difference.X));
+            float offset = MathHelper.ToDegrees((float)rad);
+                        
             intensity += 0.14f;
 
-            if (intensity > 30)
+            if (intensity > 30)//if the resource is in transit to the collector
             {
                 base.MoveToCollector();
+
+                spin += 15f;
+                rotation = new Vector3(xOffset, 0f, spin);
             }
-            else if (collector.isDying)
+            else if (collector.isDying)//if the collector ceases to exist
             {
                 intensity -= 0.25f;
-                if (intensity < 0) collector = null;
+                if (intensity < 0) collector = null;                
+            }
+            else//if the resources are in range to be collected
+            {
+                //sinAngle += 5f + intensity;
+                rotation = new Vector3(10 * ((float)Math.Sin((double)(Math.PI * (sinAngle + 30) / 180))) , 0f , offset + (45 * ((float)Math.Sin((double)(Math.PI * sinAngle / 180)))));
+
             }
         }
     }
