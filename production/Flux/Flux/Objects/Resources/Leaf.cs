9444c9f970d8be09a0990b781749927d6c959131
pyoung2 <devnull@localhost>
2013-04-06 11:54:49 -0400

    Paul's rotation code. Not sure if it's been replaced but commiting for safety.

--- production/Flux/Flux/Objects/Resources/Leaf.cs
+++ production/Flux/Flux/Objects/Resources/Leaf.cs
@@ -23,16 +23,28 @@ namespace Flux
         private Random random = new Random();
         private float xOffset;
 
-        public Leaf(Vector3 location, string modelName, float speed = 0.05f) : base(location, modelName, speed)
+        public Leaf(Vector3 location, string modelName, Vector3 parentLocation, float speed = 0.05f) : base(location, modelName, speed)
         {
             sinAngle = 0f;
             intensity = 0f;
             spin = 0f;
             xOffset = (float)random.Next(0, 20);
+
+            //X is up
+            Vector3 treeCentre = parentLocation + new Vector3(0, 2, 0);
+            Vector3 midToLeaf = (location - treeCentre);
+            float leafTilt = (float)Math.Atan(midToLeaf.Y / midToLeaf.X);
+            //Vector3 starBurt = Vector3.Dot();
+
+            rotation = new Vector3(0f, 0f, (float)(leafTilt / (2 * Math.PI)) * 180);
+
+            //debuf redundancy
+            rotation = rotation;
         }
 
         protected override void MoveToCollector()
         {
+
             Vector2 difference = collector.position - position;
             double rad = Math.Atan2((difference.Y), (difference.X));
             float offset = MathHelper.ToDegrees((float)rad);
