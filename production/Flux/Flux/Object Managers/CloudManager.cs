2682f94f9975d9ecf8552cf82ea7e0229cc34487
Jahfer <devnull@localhost>
2013-04-10 11:18:20 -0400

    Updated cloud models and positions

--- production/Flux/Flux/Object Managers/CloudManager.cs
+++ production/Flux/Flux/Object Managers/CloudManager.cs
@@ -26,17 +26,16 @@ namespace Flux
 
             clouds[0] = new Cloud(models[0], new Vector3(-20, /*8:15*/8, /*-20:-35*/-30));
             clouds[1] = new Cloud(models[1], new Vector3(50, /*8:15*/11, /*-20:-35*/-35));
-            clouds[2] = new Cloud(models[2], new Vector3(100, /*8:15*/12, /*-20:-35*/-25));
-            clouds[3] = new Cloud(models[3], new Vector3(170, /*8:15*/9, /*-20:-35*/-25));
-            clouds[4] = new Cloud(models[4], new Vector3(120, /*8:15*/11, /*-20:-35*/-35));
+            clouds[2] = new Cloud(models[2], new Vector3(100, /*8:15*/9, /*-20:-35*/-25));
+            clouds[3] = new Cloud(models[3], new Vector3(160, /*8:15*/10, /*-20:-35*/-25));
+            clouds[4] = new Cloud(models[4], new Vector3(135, /*8:15*/11, /*-20:-35*/-35));
             clouds[5] = new Cloud(models[0], new Vector3(10, /*8:15*/12, /*-20:-35*/-32));
-            clouds[6] = new Cloud(models[1], new Vector3(110, /*8:15*/9, /*-20:-35*/-28));
-            clouds[7] = new Cloud(models[2], new Vector3(190, /*8:15*/9, /*-20:-35*/-28));
+            clouds[6] = new Cloud(models[2], new Vector3(190, /*8:15*/9, /*-20:-35*/-28));
         }
 
         public static void Update()
         {
-            for (int i = 0; i < 8; i++)
+            for (int i = 0; i < 7; i++)
             {
                 clouds[i].Update();
             }
@@ -44,7 +43,7 @@ namespace Flux
 
         public static void Draw(GameTime gameTime)
         {
-            for (int i = 0; i < 8; i++)
+            for (int i = 0; i < 7; i++)
             {
                 clouds[i].Draw();
             }
